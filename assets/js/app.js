508de6bd84d670b837b975be91d76c6b56018348
Roman <rchvalbo@knights.ucf.edu>
2017-09-22 00:41:49 -0400

    added particles design

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,165 @@
 // CORE APP.JS
+
+
+var centeredSearch = $(".center-form");
+var mainCont = $(".main-cont");
+var searchButton = $("#troll-search > button");
+var buttonText = $(".troll-butt-text");
+
+centeredSearch.hide();
+
+$(document).ready(function(){
+    getVertMargin(centeredSearch, mainCont);
+    getHorzMargin(buttonText, searchButton);
+    centeredSearch.show();
+    
+});
+
+
+//align an element vertically
+function getVertMargin(smallElem, parentElem) {
+    var largeHeight = parseInt($(parentElem).height());
+    var smallHeight = parseInt($(smallElem).css("height"));
+    var marginAvail = largeHeight - smallHeight;
+    console.log(marginAvail);
+    var marginToSet = (marginAvail / 2);
+
+    $(smallElem).css("margin-top", marginToSet);
+    $(smallElem).css("margin-bottom", marginToSet);
+    
+}
+
+//align an element horizontally
+function getHorzMargin(smallElem, parentElem) {
+    var largeWidth = parseInt($(parentElem).width());
+    var smallWidth = parseInt($(smallElem).css("width"));
+    var marginAvail = largeWidth - smallWidth;
+    console.log(marginAvail);
+    var marginToSet = (marginAvail / 2);
+
+    $(smallElem).css("margin-left", marginToSet);
+    $(smallElem).css("margin-right", marginToSet);
+}
+
+
+
+particlesJS('particles-js',
+  
+  {
+    "particles": {
+      "number": {
+        "value": 80,
+        "density": {
+          "enable": true,
+          "value_area": 1000
+        }
+      },
+      "color": {
+        "value": "#28cde0"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#28cde0"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 5,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#28cde0",
+        "opacity": 0.3,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 3,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "attract": {
+          "enable": true,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 200,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 100,
+          "size": 20,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 100
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true,
+    "config_demo": {
+      "hide_card": false,
+      "background_color": "#b61924",
+      "background_image": "",
+      "background_position": "50% 50%",
+      "background_repeat": "no-repeat",
+      "background_size": "cover"
+    }
+  }
+
+);
