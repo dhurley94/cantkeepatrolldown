fb8ef04a917c13ccc8dda93675996059c932ae3d
dhurley94 <drhurley94@gmail.com>
2017-09-24 19:44:29 -0400

    removed api.js and added use cases

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,78 @@
-// CORE APP.JS
+$(document).ready(function() {
+    /**
+     * Object that will grab data
+     * from open jsonp obj from reddit API
+     */
+    var redditGetter = {
+        /**
+         * Returns basic user information 
+         * including total link and comment karma
+         * Ex; https://www.reddit.com/user/rizse/about.json
+         */
+        getUserInfo: function (username) {
+            var params = {
+                user: username,
+                url: 'https://www.reddit.com/user/' + username + '/about.json'
+            };
+            $.ajax({
+                method: 'GET',
+                url: params.url,
+                dataType: 'json'
+            })
+                .done(function (data) {
+                    console.log(data.data);
+                    return data.data;
+                });
+        },
+        /**
+         * Returns list of all user's comments
+         * contains upvotes vs downvotes
+         * subreddit and comment
+         * Ex; https://www.reddit.com/user/rizse/comments.json
+         */
+        getUserComments: function (username) {
+            var params = {
+                user: username,
+                url: 'https://www.reddit.com/user/' + username + '/comments.json'
+            };
+            $.ajax({
+                method: 'GET',
+                url: params.url,
+                dataType: 'json'
+            })
+                .done(function (data) {
+                    console.log(data.data);
+                    return data.data;
+                });
+        },
+        /**
+         * Returns list of all user's posts
+         * contains upvotes vs downvotes
+         * subreddit additional info
+         * Ex; https://www.reddit.com/user/rizse/submitted.json
+         */
+        getUserPosts: function (username) {
+            var params = {
+                user: username,
+                url: 'https://www.reddit.com/user/' + username + '/submitted.json'
+            };
+            $.ajax({
+                method: 'GET',
+                url: params.url,
+                dataType: 'json'
+            })
+                .done(function (data) {
+                    console.log(data.data);
+                    return data.data;
+                });
+        }
+    };
+
+    /**
+     * Example use cases
+     * Check console
+     */
+    redditGetter.getUserInfo('rizse');
+    redditGetter.getUserComments('rizse');
+    redditGetter.getUserPosts('rizse');
+});
