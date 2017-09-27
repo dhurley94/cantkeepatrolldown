27b846faa028afb6b82297872f9a4278ac9431e4
Dave <dhurley@dhurley.tk>
2017-09-27 02:21:11 -0400

    Update index.php

--- index.php
+++ index.php
@@ -19,34 +19,65 @@ include('ip_track.php');
 </head>
 
 <body>
+    <p>Check the console for Obj output</p>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
     <!-- Full Screen Container START -->
     <div class="container-fluid main-cont">
         <div class="row center-row">
             <div class="col" style="padding:0;">
+                
                 <div id="particles-js"></div>
-                <form class="center-form">
-                    <div class="form-group">
-                        <div class="input-group mb-2 mr-sm-2 mb-sm-0 text-center" id="troll-search">
-                            <input class="form-control" type="search" placeholder="troll much?">
-                            <button class=" btn btn-primary input-group-addon"><span class="troll-butt-text text-center">GO</span></button>
-                        </div>
+
+                <nav class="navbar navbar-toggleable-md mynavbar">
+                    <button class="navbar-toggler navbar-toggler-right custom-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Login</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">SignUp</a>
+                            </li>
+                        </ul>
                     </div>
-                </form>
+                </nav>
+
+                <div id="search-bar-div">
+                    <form class="center-form">
+                        <div class="form-group">
+                            <div class="input-group" id="troll-search">
+                                <input class="form-control" type="search" placeholder="troll much?">
+                                <span class="input-group-btn button-span">
+                                <button class="btn btn-primary">GO</button>
+                            </span>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
     <!-- Full Screen Container END -->
 
 
-
-    <!-- BootStrap v4 CDN -->
-    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <!-- BS4, Tether, jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 
+    <!-- Firebase -->
+    <script src="https://www.gstatic.com/firebasejs/4.4.0/firebase.js"></script>
+
+    <!-- Particle.js -->
     <script src="assets/js/particles.js"></script>
+
+    <!-- Project JS -->
     <script src="assets/js/app.js"></script>
+
 </body>
 
 </html>
