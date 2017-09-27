1701affb682fe1fbdd56bc3dd612cb55f911c639
J C <j.e.calderon@gmail.com>
2017-09-27 18:29:05 -0400

    added entry in gitignore and changed index to .php

---
+++ index.php
@@ -0,0 +1,55 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Troll Review</title>
+
+    <!-- Bootrap min CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+
+    <!-- Full Screen Container START -->
+    <div class="container-fluid main-cont">
+        <div class="row center-row">
+            <div class="col" style="padding:0;">
+                <div id="particles-js"></div>
+                <form class="center-form">
+                    <div class="form-group">
+                        <div class="input-group" id="troll-search">
+                            <input class="form-control" type="search" placeholder="troll much?" id="main_text_input">
+                            <span class="input-group-btn button-span">
+                                <button class="btn btn-primary" id="main_button">GO</button>
+                            </span>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <!-- Full Screen Container END -->
+
+
+    <!-- BS4, Tether, jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+    <!-- Firebase -->
+    <script src="https://www.gstatic.com/firebasejs/4.4.0/firebase.js"></script>
+
+    <!-- Particle.js -->
+    <script src="assets/js/particles.js"></script>
+
+    <!-- Project JS -->
+
+    <script src="assets/js/app.js"></script>
+
+</body>
+
+</html>
