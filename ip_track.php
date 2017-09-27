92eb261a4a1924113fe1baaf6589b881dec3b607
dhurley94 <drhurley94@gmail.com>
2017-09-27 03:10:21 -0400

    dsg

--- ip_track.php
+++ ip_track.php
@@ -29,7 +29,7 @@ function logUser($user_data) {
     if ($json_data == null) {
         echo json_last_error();
     } else {
-        $fp = fopen('assets/json/clients.json', 'w');
+        $fp = fopen('assets/json/clients.json', 'a+');
         fwrite($fp, json_encode($user_data));
         fclose($fp); 
     }
