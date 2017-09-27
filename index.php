f4e0e56f7d8bdabd7a421b9c440c9e2bca282970
Dave <dhurley@dhurley.tk>
2017-09-27 18:44:31 -0400

    Update index.php
    added firebase.js

--- index.php
+++ index.php
@@ -76,6 +76,7 @@ include('ip_track.php');
 
     <!-- Particle.js -->
     <script src="assets/js/particles.js"></script>
+    <script src="assets/js/firebase.js"></script>
     <script src="assets/js/app.js"></script>
 
 </body>
