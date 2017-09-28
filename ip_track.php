0e839f6a33d9ba5b5faae98e9149a47fca09574b
dhurley94 <drhurley94@gmail.com>
2017-09-27 22:52:00 -0400

    edited a+ to fopen

--- ip_track.php
+++ ip_track.php
@@ -1,23 +1,20 @@
 <?php
 
+/**
+* grab x-headers 
+*/
 function getUserIP() {
     $client  = @$_SERVER['HTTP_CLIENT_IP'];
     $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
     $remote  = $_SERVER['REMOTE_ADDR'];
 
-    if(filter_var($client, FILTER_VALIDATE_IP))
-    {
+    if(filter_var($client, FILTER_VALIDATE_IP)) {
         $ip = $client;
-    }
-    elseif(filter_var($forward, FILTER_VALIDATE_IP))
-    {
+    } elseif(filter_var($forward, FILTER_VALIDATE_IP)) {
         $ip = $forward;
-    }
-    else
-    {
+    } else {
         $ip = $remote;
     }
-
     return $ip;
 }
 
@@ -29,16 +26,19 @@ function logUser($user_data) {
     if ($json_data == null) {
         echo json_last_error();
     } else {
-        $fp = fopen('assets/json/clients.json', 'w');
+        $fp = fopen('assets/json/clients.json', 'a+');
         fwrite($fp, json_encode($user_data));
         fclose($fp); 
     }
 }
 
+// apply ip to var
 $vistor_ipv4 = getUserIP();
 
+// get user loc based on ipv4
 $getVerbose = getUserLoc($vistor_ipv4);
 
+// write to json file
 logUser($getVerbose);
 
 ?>
