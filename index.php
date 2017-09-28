7f6e2e7f8678b264da584169ba1268feed376647
dhurley94 <drhurley94@gmail.com>
2017-09-27 22:04:15 -0400

    removed firebase duplicate

--- index.php
+++ index.php
@@ -22,9 +22,6 @@ include('ip_track.php');
 </head>
 
 <body>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-
 
     <!-- Full Screen Container START -->
     <div class="container-fluid main-cont">
@@ -69,7 +66,8 @@ include('ip_track.php');
 
 
     <!-- BS4, Tether, jQuery -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 
